84dd5032bbf19947648c8f047d7459a38b2c39ca
yoonhye-ju <93234942+yoonhye-ju@users.noreply.github.com>
2022-01-08 16:28:18 +0900

    revise
    revise

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+	var bn = $(".box").length;
+	$(".box").on("mousewheel DOMMouseScroll", function (e) {
+		e.preventDefault();
+		var delta = 0;
+		var event = 0;
+		if (!event) event = window.event;
+		if (event.wheelDelta) {
+			delta = event.wheelDelta / 120;
+			if (window.opera) delta = -delta;
+		} else if (event.detail) delta = -event.detail / 3;
+
+		if (delta < 0) {
+			n = $(this).index() + 1;
+		} else {
+			n = $(this).index() - 1;
+		}
+		console.log(n)
+		if (n >= bn) {
+			n = bn - 1;
+		} else if (n <= -1) {
+			n = 0
+		}
+		$(".wrap").stop().animate({ left: -n * 100 + "%" });
+	});
+});
+
